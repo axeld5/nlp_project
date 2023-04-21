59fe2ecdbaa47ad6f9d384ef2de16c8efce79ef5
axeld5 <axel.darmouni@student-cs.fr>
2023-04-21 10:10:11 +0200

    found the issue

--- nlp_assignment/src/train_utils.py
+++ nlp_assignment/src/train_utils.py
@@ -1,13 +1,9 @@
 import pandas as pd 
 import numpy as np
-import torch
 
 from datasets import Dataset
 from datasets.dataset_dict import DatasetDict
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
-from torch.utils.data import DataLoader
-from torch.nn.utils.rnn import pad_sequence
-from tqdm import tqdm
 from transformers import (
     DataCollatorForSeq2Seq, AutoTokenizer, BartForConditionalGeneration,
     Seq2SeqTrainingArguments, Trainer
@@ -246,7 +242,6 @@ def train_model(
 
     # Tokenize Dataset
     id_ds, id_tokenized_ds = loader.set_data_for_training_semeval(t5_exp.tokenize_function_inputs)
-
     # Training arguments
     training_args = {
         'output_dir':'.',
